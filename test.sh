872263d9a5a4f9b448a4ff664aecdb72c6f66302
Maxwell Krohn <themax@gmail.com>
2017-11-03 11:05:18 -0400

    second warp -> warp test

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 iced btctx.iced \
-	--prev-tx fa6ba761df4b57bf323f5edec522a605a4fcebf300468af5d86f113f6a355adc:0 \
-	--prev-addr 17MSZVRqSrWfwVq5n7jkcjf9dkePwTkRBM \
-	--send-to 1LLmEMhqHP264Qqpv83Sf3s9hhSicJXtXK \
-	--fee 3 --approx-btc-price 7300 --approx-value 30
+	--prev-tx 3c5556c9bef2aa4a3eecccf4c83f442ff74fb91049ca6ac9e877fdef24bf0831:1 \
+	--prev-addr 1BpJ5vHNVsxbxufCce8gQtkEYsTdAAfjQd \
+	--send-to 1LPWQbqPYAtvuGij8W2KYoxNkUABgrDfkF \
+	--fee 3 --approx-btc-price 7300 --approx-value 300
