e1341c78e33f8df16f709236ae1cae08ed4687b9
Maxwell Krohn <themax@gmail.com>
2017-11-03 05:53:30 -0400

    preliminary work

---
+++ test.sh
@@ -0,0 +1,6 @@
+iced btctx.iced \
+	--prev-tx 485ef252bb85f13e25a7bc6e450389fdb44c2d08c727ebf8dd9db6571fe844cd:0 \
+	--send-to 1BpJ5vHNVsxbxufCce8gQtkEYsTdAAfjQd:500 \
+	--send-to 1LPWQbqPYAtvuGij8W2KYoxNkUABgrDfkF:500 \
+	--prev-addr 1KgUyTokuc24GAfeymeG87dZmKbY8e9Vjy \
+	--fee 1.00 --btc-price 7300
